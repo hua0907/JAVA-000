931be7af872c4b0afe306b7dd2b9e311b21bb9c8
huadeyi <hdy1101496521@163.com>
2020-10-20 18:23:23 +0800

    第一周作业： 1、自定义classLoad类加载器 2、内存关系图

---
+++ Week_01/work/src/com/hua/study/jvm/MyClassLoader.java
@@ -0,0 +1,60 @@
+package com.hua.study.jvm;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MyClassLoader extends ClassLoader {
+
+    private final static String PATH = "C:\\gitwork\\park\\jvm\\src\\main\\java\\com\\hua\\jvm\\work\\Hello.xlass";
+    private final static String CLASS_NAME = "Hello";
+    private final static String METHOD_NAME = "hello";
+
+    public static void main(String[] args) {
+
+        try {
+            Class<?> aClass = new MyClassLoader().findClass(CLASS_NAME);
+            Method helloMethod = aClass.getDeclaredMethod(METHOD_NAME);
+            helloMethod.invoke(aClass.newInstance());
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) {
+        byte[] bytes = getByteArray(PATH);
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    private byte[] getByteArray(String path) {
+        FileInputStream fio = null;
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        ;
+        try {
+            fio = new FileInputStream(path);
+            int len;
+            int realCode;
+            while ((len = fio.read()) != -1) {
+                realCode = 255 - len;
+                bao.write(realCode);
+            }
+            bao.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fio) {
+                    fio.close();
+                }
+                bao.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return bao.toByteArray();
+    }
+
+}
