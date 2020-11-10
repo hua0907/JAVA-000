29d85b0b087c4a51e19c65dbd95822f6eff169c4
hua0907 <hdy1101496521@163.com>
2020-11-11 00:32:29 +0800

    Future FutureTask countDownLatch

--- Week_04/homework/src/main/java/com/study/hua/demo/CountDownLatchDemo.java
+++ Week_04/homework/src/main/java/com/study/hua/demo/CountDownLatchDemo.java
@@ -1,6 +1,7 @@
 package com.study.hua.demo;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
@@ -9,29 +10,27 @@ import java.util.concurrent.*;
  *
  * 一个简单的代码参考：
  */
-public class CallableDemo {
-    
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+public class CountDownLatchDemo {
+    private static int result;
+    public static void main(String[] args) throws InterruptedException {
         
         long start=System.currentTimeMillis();
         // 在这里创建一个线程或线程池，
-        ExecutorService pool = Executors.newSingleThreadExecutor();
-
         // 异步执行 下面方法
-        Future<Integer> future = pool.submit(new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
-                return sum();
-            }
-        });
-        int result = future.get(); //这是得到的返回值
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        new Thread(()->{
+            result = sum();
+            countDownLatch.countDown();
+        }).start();
+        countDownLatch.await();
+         //这是得到的返回值
+
         // 确保  拿到result 并输出
         System.out.println("异步计算结果为："+result);
          
         System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
         
         // 然后退出main线程
-        pool.shutdown();
     }
     
     private static int sum() {
